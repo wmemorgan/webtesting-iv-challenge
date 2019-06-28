784b319edf9429eacdffd8339a753dcaf0841041
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-28 10:32:08 +0800

    Add option for server environment

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -3,4 +3,5 @@ const knex = require('knex')
 
 const knexConfig = require('../knexfile')
 
-module.exports = knex(knexConfig[process.env.NODE_ENV || 'development'])
+module.exports = knex(knexConfig[process.env.DB_ENV || 
+  process.env.NODE_ENV || 'development'])
